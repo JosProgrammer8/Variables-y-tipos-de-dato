4df120d2ced2ab555785fc082e8e640d00695510
Jose Enrique <108635029+JosProgrammer8@users.noreply.github.com>
2022-07-09 15:54:03 -0500

    Add files via upload

---
+++ vtd_ejercicio2.py
@@ -0,0 +1,7 @@
+Num = 1
+Num2 = 4 
+Num3 = 7
+
+print(type(Num))
+print(type(Num2))
+print(type(Num3))
---
+++ vtd_ejercicio3.py
@@ -0,0 +1,12 @@
+a = "h"
+b = "o"
+c = "l"
+d = "a"
+e = " "
+f = "a"
+g = "m"
+h = "i"
+i = "g"
+j = "o"
+
+print(a,b,c,d,e,f,g,h,i,j)
